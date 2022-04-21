f806c414cb3cc8ca99823bd00396b791f951052a
gbelther <gabriel.belther@squadts.com.br>
2022-04-21 12:59:38 -0300

    feature: criação do story do button

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Button } from ".";
+
+export default {
+  title: "Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>{args.children}</Button>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: "Button Primary",
+};
