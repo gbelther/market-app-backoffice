2a329180a95b4bd0aa3f488c14b32c646f16bd7b
gbelther <gabriel.belther@squadts.com.br>
2022-04-21 12:57:42 -0300

    feature: criação do componente de button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,40 @@
+import { ButtonHTMLAttributes, ReactNode, useMemo } from "react";
+import { IColors } from "../../interfaces/IColors";
+
+import "./styles.scss";
+
+export type ButtonSize = "small" | "normal" | "large";
+
+interface IButtonProps {
+  bgColor?: IColors;
+  color?: IColors;
+  borderColor?: IColors;
+  size?: ButtonSize;
+  children: ReactNode;
+  buttonProps?: ButtonHTMLAttributes<HTMLButtonElement>;
+}
+
+const Button = ({
+  bgColor = "main",
+  color = "dark-01",
+  borderColor,
+  size = "large",
+  children,
+  buttonProps,
+}: IButtonProps) => {
+  const buttonClassName = useMemo(() => {
+    const classNameSize = `size-${size}`;
+
+    return `c-button c-button-${classNameSize} bg-${bgColor} color-${color} border-color-${
+      borderColor ?? bgColor
+    }  ${buttonProps?.className ?? ""}`;
+  }, [size, bgColor, color, borderColor, buttonProps?.className]);
+
+  return (
+    <button {...buttonProps} className={buttonClassName}>
+      {children}
+    </button>
+  );
+};
+
+export { Button };
---
+++ src/interfaces/IColors.ts
@@ -0,0 +1,18 @@
+export type IColors =
+  | "main"
+  | "main-dark"
+  | "main-extra-dark"
+  | "dark-01"
+  | "dark-02"
+  | "dark-03"
+  | "dark-04"
+  | "light-01"
+  | "light-02"
+  | "light-03"
+  | "light-04"
+  | "error"
+  | "error-light"
+  | "error-dark"
+  | "success"
+  | "success-light"
+  | "success-dark";
