aa3060241bf9a9792b3ff3f866d54374e25991bf
gbelther <gabriel.belther@squadts.com.br>
2022-04-28 23:28:49 -0300

    feature: implementação do login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,32 +1,101 @@
+import { useMemo } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { createSession } from "../../store/user/thunks/createSession";
+import { IInputFeedback } from "../../interfaces/IInputFeedback";
 import { Button } from "../../components/Button";
 import { InputText } from "../../components/InputText";
 
 import "./styles.scss";
 
+interface IFormLogin {
+  email: string;
+  password: string;
+}
+
+const loginFormSchema = yup.object({
+  email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
+  password: yup.string().required("Campo obrigatório"),
+});
+
 const Login = () => {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormLogin>({
+    resolver: yupResolver(loginFormSchema),
+  });
+
+  const onSubmit = async ({ email, password }: IFormLogin) => {
+    dispatch(
+      createSession({
+        email,
+        password,
+      })
+    );
+  };
+
+  const feedbackEmail = useMemo(() => {
+    if (errors.email) {
+      return {
+        type: "error",
+        message: errors.email.message ?? "",
+      } as IInputFeedback;
+    }
+  }, [errors.email]);
+
+  const feedbackPassword = useMemo(() => {
+    if (errors.password) {
+      return {
+        type: "error",
+        message: errors.password.message ?? "",
+      } as IInputFeedback;
+    }
+  }, [errors.password]);
+
   return (
     <section id="pg-login" className="f-centered">
-      <div className="pg-login__form">
+      <form
+        className="pg-login__form"
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+      >
         <p className="pg-login__form--title">Faça seu login</p>
+
         <div className="d-flex f-col gap-4">
           <InputText
             label="E-mail"
             inputProps={{
               type: "email",
+              ...register("email"),
             }}
+            feedback={feedbackEmail}
           />
           <InputText
             label="Senha"
             inputProps={{
               type: "password",
+              ...register("password"),
             }}
+            feedback={feedbackPassword}
           />
+        </div>
+        <div>
+          <p className="message-error-sm">{user.error?.message}</p>
         </div>
         <div className="pg-login__form__buttons-wrapper">
           <div className="flex-1">
             <Button
               bgColor="main-extra-dark"
               color="light-01"
+              loading={user.loading}
               buttonProps={{
                 className: "w-100",
               }}
@@ -35,10 +104,17 @@ const Login = () => {
             </Button>
           </div>
           <div>
-            <Button bgColor="light-02">Cadastre-se</Button>
+            <Button
+              bgColor="light-02"
+              buttonProps={{
+                type: "submit",
+              }}
+            >
+              Cadastre-se
+            </Button>
           </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
